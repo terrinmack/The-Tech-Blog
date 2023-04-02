abb343174ff0a3687b679e2d14e2f7631cc84f34
Your Name <you@example.com>
2023-04-01 18:36:42 -0700

    working on fun seeds data

--- seeds/postData.js
+++ seeds/postData.js
@@ -3,9 +3,24 @@ const { Post } = require('../models');
 const postdata =
 [
   {
-    "postTitle": "Mushroom Day",
+    "postTitle": "Mushroom extravaganza",
     "postContent": "Today is a great day to eat shiitake mushrooms",
-    "userId": 1
+    "userId": 4
+  },
+  {
+    "postTitle": "Clear Day!",
+    "postContent": "We cleared p8s today!",
+    "userId": 2
+  },
+  {
+    "postTitle": "Where did he go?",
+    "postContent": "When will Aura hide in my kangaroo pouch?",
+    "userId": 6
+  },
+  {
+    "postTitle": "I think I am thirsty",
+    "postContent": "What is your favorite type of milk?",
+    "userId": 7
   },
 ];
 
--- seeds/userData.js
+++ seeds/userData.js
@@ -10,6 +10,30 @@ const userdata =
     "username": "Pyrrha",
     "password": "stampede"
   },
+  {
+    "username": "Aeldreth",
+    "password": "japanese"
+  },
+  {
+    "username": "Kira",
+    "password": "machinist"
+  },
+  {
+    "username": "Alyssa",
+    "password": "snatcher"
+  },
+  {
+    "username": "Cedar",
+    "password": "kangaroo"
+  },
+  {
+    "username": "Aura",
+    "password": "thailand"
+  },
+  {
+    "username": "Zeiden",
+    "password": "samurai"
+  },
 ]
 
 const seedUser = () => User.bulkCreate(userdata);
