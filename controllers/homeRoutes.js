75f8d6aec121c2315859c0d577239a8c4cde23d2
Your Name <you@example.com>
2023-04-03 22:34:48 -0700

    added update code within models user.js file. Worked on logged in authorization for finding/obtaining posts in homeRoutes.js folder

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -3,34 +3,33 @@ const { User, Post, Comment } = require('../models');
 const withAuth = require('../utils/auth');
 
 router.get('/', async (req, res) => {
-    Post.findAll({
-        attributes: [
-            'id',
-            'postTitle',
-            'postContent',
-            'createdAt'
-        ],
-        include: [
-            {
-            model: Comment,
-            attributes: ['id', 'userId', 'postId', 'commentContent', 'createdAt'],
-            include: {
-                model: User,
-                attributes: ['username']
-            }
-        },
-        {
-            model: User,
-            attributes: ['username']
+  Post.findAll({
+    attributes: ['id', 'postTitle', 'postContent', 'createdAt'],
+    include: [
+      {
+        model: Comment,
+        attributes: ['id', 'userId', 'postId', 'commentContent', 'createdAt'],
+        include: {
+          model: User,
+          attributes: ['username']
         }
+      },
+      {
+        model: User,
+        attributes: ['username']
+      }
     ]
-    }).then(dbPostData => {
-        const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
-    }).catch(err => {
-        console.log(err);
-        res.status(500).json(err);
+  }).then(dbPostData => {
+    const posts = dbPostData.map(post => post.get({ plain: true }));
+    res.render('homepage', {
+      posts,
+      logged_in: req.session.logged_in,
+      username: req.session.logged_in,
     });
+  }).catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 
